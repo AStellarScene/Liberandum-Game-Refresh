82d6c3cf42db1af21596528e5182eb8007fc2314
Adam L. <144614242+AStellarScene@users.noreply.github.com>
2024-05-08 15:19:02 -0400

    added lives UI - not functional yet

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,29 +1,21 @@
 extends CharacterBody2D
 
-@onready var label_lives = $"Label-Lives"
-@onready var timer = $Hitbox/Timer
-@onready var label_collect = $"Label-Collect"
+@onready var timer = $HitBox/Timer
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-var lives = 8
+var lives = 3
 var collect = 0
 var hurt = false
 
-
-
-
-
-
-
 func _physics_process(delta):
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = direction * 250
+	move_and_slide()
+	
 	var isLeft = velocity.x < 0
 	var isRight = velocity.x > 0
 	var isUp = velocity.y < 0
 	var isDown = velocity.y > 0
-	
-	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	velocity = direction * 250
-	move_and_slide()
 	
 	if isLeft:
 		animated_sprite_2d.animation = "face_left"
@@ -37,20 +29,14 @@ func _physics_process(delta):
 func _on_hitbox_area_entered(area):
 	if area.is_in_group("hurt"):
 		print("Hurt!")
-		lives = lives - 1
 		if lives > 0:
 			timer.start()
 			hurt = true
-			label_lives.text = str(lives)
 		else:
 			position = Vector2(0, 0)
-			lives = 3
-			label_lives.text = str(lives)
 			get_tree().reload_current_scene()
 	if area.is_in_group("collect"):
 		collect = collect + 1
-		label_collect.text = str(collect) + "/20"
-
 
 func _on_hit_box_area_entered(area):
 	if area.is_in_group("potion"):
